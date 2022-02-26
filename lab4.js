1cb3a3ffc3f344067fd1c6ded916fb926d40871f
eighteous <eighteous@gmail.com>
2022-02-26 16:27:52 +1100

    Update lab4.js

--- lab4.js
+++ lab4.js
@@ -1,11 +1,18 @@
 
 
-
+function makeObjects(){
 
 let user = new Object(); // "object constructor" syntax
 let user = {};  // "object literal" syntax
 
+}
 
+
+funtion helloWorld(){
+  
+  alert("Hello world");
+  
+}
 
 
 
